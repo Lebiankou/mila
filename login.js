eba492bb85eb600d79d945c5af5863ce3bc23117
Demetry <lebiankoudzmitry@gmail.com>
2025-01-31 18:14:16 +0300

    пофиксил адресс сервера

--- login.js
+++ login.js
@@ -312,7 +312,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             };
 
-            fetch('http://127.0.0.1:8000/signup', {
+            fetch('http://127.0.0.1:8000/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
